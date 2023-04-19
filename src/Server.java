90a576ad820316f1a2a59ac9e399792ce9f8a4f7
Konstantinos-Martinis <kostasmartin23@gmail.com>
2023-04-19 15:25:20 +0300

    Co-authored-by: ATHANASIOS STAMOGIANNOPOULOS <p3200185@aueb.gr> Co-authored-by: CHRISTOS MAKRIS <p3200097@aueb.gr>

--- src/Server.java
+++ src/Server.java
@@ -13,7 +13,30 @@ public class Server {
 
         Socket clientSocket = serverSocket.accept();
         System.out.println("Client connected.");
+        //Receive Gpx file on server
         
+        
+        InputStream is = clientSocket.getInputStream();
+        DataInputStream dis = new DataInputStream(is);
+        int length = dis.readInt();
+        byte[] gpxBytes = new byte[length];
+        dis.readFully(gpxBytes);
+        dis.close();
+        
+        
+        //Save GPX file on server
+        File gpxFile = new File("C:/Users/Martinisk/Desktop/Tracking-App/gpx files in master");
+        FileOutputStream fos = new FileOutputStream(gpxFile);
+        fos.write(gpxBytes);
+        fos.close();
+
+        clientSocket.close();
+        serverSocket.close();
+        
+        
+        
+        
+        /*
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
@@ -22,14 +45,14 @@ public class Server {
         while ((inputLine = in.readLine()) != "test") {
             System.out.println("Received message from client: " + inputLine);
             out.println("Echo: " + inputLine);
-            System.out.println("makabou");
+            System.out.println("test");
         }
 
         in.close();
         out.close();
 
         clientSocket.close();
-        serverSocket.close();
+        serverSocket.close();*/
         } catch (IOException e){
             e.printStackTrace();
         }
--- src/kinito.java
+++ src/kinito.java
@@ -18,13 +18,37 @@ public class kinito{
 			requestsocket = new Socket("127.0.0.1",4321);
 			in = new ObjectInputStream(requestsocket.getInputStream());
 			out = new ObjectOutputStream(requestsocket.getOutputStream());
+			//Read Gpx file into a Byte array
+			File gpx = new File("C:/Users/Martinisk/Desktop/Tracking-App/gpxs/route1.gpx");
+			byte [] gpxBytes = new byte[(int) gpx.length()];
+			FileInputStream fis = new FileInputStream(gpx);
+			fis.read(gpxBytes);
+			fis.close();
 
-			GPXFile t = new GPXFile("route 1", "C:/Users/Martinisk/Desktop/Tracking-App/gpxs/route1.gpx");
+
+			/*GPXFile t = new GPXFile("route 1", "C:/Users/Martinisk/Desktop/Tracking-App/gpxs/route1.gpx");
             out.writeObject(t);
 			out.writeObject("test test");
-            out.flush();
+            out.flush();*/
 
 			/* Print the received result from server */
+
+			//Send GPX file to server
+			OutputStream os = requestsocket.getOutputStream();
+			DataOutputStream dos = new DataOutputStream(os);
+			dos.writeInt(gpxBytes.length);
+			dos.write(gpxBytes);
+			dos.flush();
+			dos.close();
+	
+			requestsocket.close();	
+			in.close();	out.close();
+			requestsocket.close();
+			System.out.println("to esteile");
+
+
+
+
 			System.out.println("Server>" + in.readInt());
 
 		} catch (UnknownHostException unknownHost) {
@@ -39,5 +63,6 @@ public class kinito{
 				ioException.printStackTrace();
 			}
 		}
+		
     }    
 }
